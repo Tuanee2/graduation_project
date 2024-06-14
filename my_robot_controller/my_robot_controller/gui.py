dfedf167b7ec257bcab0831cb7003c7b702d1cd9
Tuanee2 <buiquoctuan18102002@gmail.com>
2024-06-14 16:52:27 +0700

    new feature: delete map

--- my_robot_controller/my_robot_controller/gui.py
+++ my_robot_controller/my_robot_controller/gui.py
@@ -1,7 +1,7 @@
 import os
 import signal
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, messagebox
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String, Int32, Float32
@@ -34,6 +34,8 @@ class ROS2GUI(Node):
         self.manual_control_enabled = False
         self.text_data = [
             "Bring up",
+            "Config map",
+            "Delete map",
             "Send the work area",
             "Delete the work area",
             "Send the path",
@@ -62,7 +64,7 @@ class ROS2GUI(Node):
         cosy_cosp = 1 - 2 * (orientation_q.y * orientation_q.y + orientation_q.z * orientation_q.z)
         theta = math.atan2(siny_cosp, cosy_cosp)
 
-        self.odom_feedback.config(text=f"Position:\nx={x:.2f}\ny={y:.2f}\nθ={theta:.2f}")
+        self.odom_feedback.config(text=f"Position:\nx= {x:.4f}\ny= {y:.4f}\nθ= {theta:.4f}")
 
     def acc_sub_callback(self, msg : Float32):
         self.accuracy_feedback.config(text=f"Acc : {msg.data:.2f} %")
@@ -83,7 +85,7 @@ class ROS2GUI(Node):
         self.root.grid_rowconfigure(5, weight=1, minsize=50)
         self.root.grid_rowconfigure(6, weight=1, minsize=50)
 
-        self.map_config_button = tk.Button(self.root, text="Map Config", bg="lightblue", width=25, height=2, command=self.map_config_callback)
+        self.map_config_button = tk.Button(self.root, text=self.text_data[1], bg="lightblue", width=25, height=2, command=self.map_config_callback)
         self.map_config_button.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
 
         self.map_name_entry = tk.Entry(self.root, width=25)
@@ -92,6 +94,7 @@ class ROS2GUI(Node):
         # Label 1 trở thành nút bấm
         self.button1 = tk.Button(self.root, text=self.text_data[0], bg="lightblue", width=25, height=2, command=self.button1_callback)
         self.button1.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
+        self.button1.config(state="disabled")
 
         # Label 2 trở thành khung hiển thị tin nhắn từ topic đăng ký
         self.feedback_label = tk.Label(self.root, text="Feedback: Waiting for messages...", width=25, height=2, bg="white", relief="groove", bd=2)
@@ -128,11 +131,11 @@ class ROS2GUI(Node):
         self.accuracy_feedback.grid(row=2, column=2, padx=10, pady=10, sticky="nsew")
 
         # Label 3 trở thành nút bấm
-        self.button2_0 = tk.Button(self.root, text=self.text_data[1], bg="lightblue", width=25, height=2, command=self.button2_0_callback)
+        self.button2_0 = tk.Button(self.root, text=self.text_data[3], bg="lightblue", width=25, height=2, command=self.button2_0_callback)
         self.button2_0.grid(row=3, column=0, padx=10, pady=10, sticky="nsew")
         self.button2_0.config(state="disabled")
 
-        self.button2_1 = tk.Button(self.root, text=self.text_data[5], bg="lightblue", width=25, height=2, command=self.button2_1_callback)
+        self.button2_1 = tk.Button(self.root, text=self.text_data[7], bg="lightblue", width=25, height=2, command=self.button2_1_callback)
         self.button2_1.grid(row=3, column=1, padx=10, pady=10, sticky="nsew")
         self.button2_1.config(state="disabled")
 
@@ -140,27 +143,51 @@ class ROS2GUI(Node):
         self.odom_feedback = tk.Label(self.root, text="Position: Waiting for messages...", width=25, height=6, bg="white", relief="groove", bd=2)
         self.odom_feedback.grid(row=3, column=2, rowspan=3, padx=10, pady=10, sticky="nsew")
 
-        self.button3_0 = tk.Button(self.root, text=self.text_data[3], bg="lightblue", width=25, height=2, command=self.button3_0_callback)
+        self.button3_0 = tk.Button(self.root, text=self.text_data[5], bg="lightblue", width=25, height=2, command=self.button3_0_callback)
         self.button3_0.grid(row=4, column=0, padx=10, pady=10, sticky="nsew")
         self.button3_0.config(state="disabled")
 
-        self.button3_1 = tk.Button(self.root, text=self.text_data[6], bg="lightblue", width=25, height=2, command=self.button3_1_callback)
+        self.button3_1 = tk.Button(self.root, text=self.text_data[8], bg="lightblue", width=25, height=2, command=self.button3_1_callback)
         self.button3_1.grid(row=4, column=1, padx=10, pady=10, sticky="nsew")
         self.button3_1.config(state="disabled")
 
-        self.button4_1 = tk.Button(self.root, text=self.text_data[7], bg="lightblue", width=25, height=2, command=self.button4_1_callback)
+        self.button4_1 = tk.Button(self.root, text=self.text_data[9], bg="lightblue", width=25, height=2, command=self.button4_1_callback)
         self.button4_1.grid(row=5, column=1, padx=10, pady=10, sticky="nsew")
         self.button4_1.config(state="disabled")
 
-        self.button4_0 = tk.Button(self.root, text=self.text_data[9], bg="lightblue", width=25, height=2, command=self.shutdown_gui)
+        self.button4_0 = tk.Button(self.root, text=self.text_data[11], bg="lightblue", width=25, height=2, command=self.shutdown_gui)
         self.button4_0.grid(row=5, column=0, padx=10, pady=10, sticky="nsew")
 
     def map_config_callback(self):
         map_name = self.map_name_entry.get()
         if map_name:
             self.map_name = map_name
-            self.feedback_label.config(text=f"Map set to: {map_name}")
-            self.map_config_button.config(bg="gray",state="disabled")
+            if self.map_config_button.cget('text') == self.text_data[1]:
+                self.feedback_label.config(text=f"Map set to: {map_name}")
+                self.map_config_button.config(text=self.text_data[2])
+                self.map_name_entry.config(state="disabled")
+                self.button1.config(state="normal")
+            else:
+                if messagebox.askyesno("Warning", "Deleting the map will erase all data and stop all processes. Do you want to proceed?"):
+                    self.map_config_button.config(text=self.text_data[1])
+                    self.map_name_entry.config(state="normal")
+                    self.map_name_entry.delete(0, tk.END)
+                    self.feedback_label.config(text="Map deleted.")
+                    cmd = Int32()
+                    cmd.data = 9
+                    self.publish_.publish(cmd)
+                    print(cmd.data)
+                    if self.cond1 == True:
+                        os.killpg(self.process0.pid,signal.SIGTERM)
+                    self.button1.config(bg="lightblue",state="disabled")
+                    self.button2_0.config(text=self.text_data[3],state="disabled")
+                    self.button3_0.config(text=self.text_data[5],state="disabled")
+                    self.button2_1.config(bg="lightblue",state="disabled")
+                    self.button3_1.config(bg="lightblue",state="disabled")
+                    self.button4_1.config(text=self.text_data[9],state="disabled")
+
+                
+
         else:
             self.feedback_label.config(text="Please enter a valid map name")
 
@@ -175,15 +202,13 @@ class ROS2GUI(Node):
         self.button3_0.config(state="normal")
         home_dir = os.path.expanduser("~")
         self.launch_command_1 = ["ros2", "launch", "my_launch", "my_localization.launch.py", f"map:={home_dir}/{self.map_name}.yaml"]
-        print(self.launch_command_1)
-        print(f"Running command: {self.launch_command_1}")  # Thêm lệnh in để kiểm tra
-        self.launch_command_2 = ["ros2", "run", "my_launch", "work_area_monitor"]
+        #self.launch_command_2 = ["ros2", "run", "my_launch", "work_area_monitor"]
         self.launch_command_3 = ["ros2", "run", "my_launch", "create_path"]
         self.launch_command_4 = ["ros2", "run", "my_launch", "controller_monitor"]
         self.launch_command_5 = ["ros2", "run", "my_launch", "kidnap"]
         #self.processes.append(subprocess.Popen(self.launch_command_1))
         self.process0 = subprocess.Popen(self.launch_command_1,preexec_fn=os.setsid)
-        self.process1 = subprocess.Popen(self.launch_command_2)
+        #self.process1 = subprocess.Popen(self.launch_command_2)
         self.process2 = subprocess.Popen(self.launch_command_3)
         self.process3 = subprocess.Popen(self.launch_command_4)
         self.process4 = subprocess.Popen(self.launch_command_5)
@@ -192,12 +217,12 @@ class ROS2GUI(Node):
 
     def button2_0_callback(self):
         cmd = Int32()
-        if self.button2_0.cget('text') == self.text_data[1]:
-            self.button2_0.config(text=self.text_data[2])
+        if self.button2_0.cget('text') == self.text_data[3]:
+            self.button2_0.config(text=self.text_data[4])
             cmd.data = 3
             self.publish_.publish(cmd)
         else:
-            self.button2_0.config(text=self.text_data[1])
+            self.button2_0.config(text=self.text_data[3])
             cmd.data = 4
             self.publish_.publish(cmd)
 
@@ -213,15 +238,15 @@ class ROS2GUI(Node):
 
     def button3_0_callback(self):
         cmd = Int32()
-        if self.button3_0.cget('text') == self.text_data[3]:
-            self.button3_0.config(text=self.text_data[4])
+        if self.button3_0.cget('text') == self.text_data[5]:
+            self.button3_0.config(text=self.text_data[6])
             cmd.data = 1
             self.publish_.publish(cmd)
             self.button2_1.config(state="normal")
             self.button3_1.config(state="normal")
             self.button4_1.config(state="normal")
         else:
-            self.button3_0.config(text=self.text_data[3])
+            self.button3_0.config(text=self.text_data[5])
             self.button2_1.config(bg="lightblue",state="disabled")
             self.button3_1.config(bg="lightblue",state="disabled")
             self.button4_1.config(state="disabled")
@@ -242,12 +267,12 @@ class ROS2GUI(Node):
     def button4_1_callback(self):
         cmd = Int32()
 
-        if self.button4_1.cget('text') == self.text_data[7]:
-            self.button4_1.config(text=self.text_data[8])
+        if self.button4_1.cget('text') == self.text_data[9]:
+            self.button4_1.config(text=self.text_data[10])
             cmd.data = 7
             self.publish_.publish(cmd)
         else:
-            self.button4_1.config(text=self.text_data[7])
+            self.button4_1.config(text=self.text_data[9])
             cmd.data = 8
             self.publish_.publish(cmd)
     
